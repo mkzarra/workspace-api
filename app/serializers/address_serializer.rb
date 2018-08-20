c25fa6927c793ebbff3bd76a18458747d4737be2
mkzarra <mkzarra@gmail.com>
2018-08-20 11:18:56 -0400

    added relationship with stores and addresses in serializers

--- app/serializers/address_serializer.rb
+++ app/serializers/address_serializer.rb
@@ -1,3 +1,4 @@
 class AddressSerializer < ActiveModel::Serializer
-  attributes :id, :line_one, :line_two, :city, :state, :zip_code, :store
+  attributes :id, :line_one, :line_two, :city, :state, :zip_code
+  belongs_to :store
 end
