46e9f933f20856fe627d0dd86ca6859d5894b8eb
mkzarra <mkzarra@gmail.com>
2018-12-13 10:40:09 -0500

    changes made to addresses_controller.rb

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -3,21 +3,23 @@ class AddressesController < ApplicationController
 
   # GET /addresses
   def index
-    if params.key?(:line_one, :line_two, :city, :state, :zip_code)
-    @addresses = Address.all.select do |address|
-      next if address.line_one.nil?
-      address.line_one.downcase.include? params [:line_one].downcase
-      next if address.line_two.nil?
-      address.line_two.downcase.include? params [:line_two].downcase
-      next if address.city.nil?
-      address.city.downcase.include? params [:city].downcase
-      next if address.state.nil?
-      address.state.downcase.include? params [:state].downcase
-      next if address.zip_code.nil?
-      address.zip_code.downcase.include? params [:zip_code].downcase
+    if params.keys('line_one', 'line_two', 'city', 'state', 'zip_code')
+      @addresses = Address.all.select do |address|
+        next if address.line_one.nil?
+        address.line_one.downcase.include?(params['line_one'].downcase)
+        next if address.line_two.nil?
+        address.line_two.downcase.include?(params['line_two'].downcase)
+        next if address.city.nil?
+        address.city.downcase.include?(params['city'].downcase)
+        next if address.state.nil?
+        address.state.downcase.include?(params['state'].downcase)
+        next if address.zip_code.nil?
+        address.zip_code.downcase.include?(params['zip_code'].downcase)
+      end
+      render json: @addresses
+    else
+      render json: Address.all
     end
-    render json: @addresses
-  end
   end
 
   # GET /addresses/1
@@ -54,7 +56,11 @@ class AddressesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_address
-      @address = Address.find(params[:id])
+      if params[:id]
+        @address = Address.find(params[:id])
+      elsif address_params[:line_one] && address_params[:line_two] && address_params[:city] && address_params[:state] && address_params[:zip_code] && address_params[:store_id]
+        @address = Address.find_by(line_one: address_params[:line_one], line_two: address_params[:line_two], city: address_params[:city], state: address_params[:state], zip_code: address_params[:zip_code], store_id: address_params[:store_id])
+      end
     end
 
     # Only allow a trusted parameter "white list" through.
